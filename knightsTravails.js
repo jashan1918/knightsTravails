210a784766b96be31b604fa7d10a4f6e5d95bdd5
Jashan <jashandeep4329@gmail.com>
2025-10-14 18:30:48 +0530

    Add initial implementation of knight's travel algorithm and package configuration

--- knightsTravails.js
+++ knightsTravails.js
@@ -1,2 +1,25 @@
 console.log("start!");
 
+function minKnightMoves(start, end) {
+
+console.log(start);
+console.log(end);
+}
+
+const getMoves = (x, y) => {
+  const moves = [
+                 [x + 1, y + 2], [x + 2, y + 1],
+                 [x + 2, y - 1], [x + 1, y - 2],
+                 [x - 1, y - 2], [x - 2, y - 1],
+                 [x - 2, y + 1], [x - 1, y + 2] 
+                ]
+
+   return moves;
+
+}
+
+const start = [3,3];
+const end = [4,4];
+
+minKnightMoves(start, end);
+
