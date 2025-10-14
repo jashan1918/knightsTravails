8735416fbc9ebbc8b4c1f35381fc7ed54ce377bf
Jashan <jashandeep4329@gmail.com>
2025-10-15 00:15:55 +0530

    Refactor minKnightMoves function to improve pathfinding logic and reset start/end positions

--- knightsTravails.js
+++ knightsTravails.js
@@ -1,9 +1,53 @@
 console.log("start!");
 
 function minKnightMoves(start, end) {
+  const path = []
 
-console.log(start);
-console.log(end);
+
+  let queue = [];
+  let current = start;
+  queue.push(start);
+  let visited = Array.from({length : 8}, () => Array(8).fill(false))
+      visited[current[0]][current[1]] = true;
+    // queue.shift(start);
+
+    
+    console.log(queue);
+
+    function isInsideBoard(nextX, nextY) {
+  return nextX >= 0 && nextX <= 7 && nextY >= 0 && nextY <= 7;
+    }
+    
+    while(queue.length) {
+      current = queue.shift();
+      path.push(current);
+      
+
+      if(current[0] === end[0] && current[1] === end[1]) {
+        console.log("Reached the end!", path);
+        break;
+      }
+
+        const possibleMoves = getMoves(current[0], current[1]);
+  console.log(possibleMoves);
+
+  possibleMoves.forEach((e) => queue.push(e));
+  console.log(queue);
+
+  for (const [moveX, moveY] of possibleMoves) {
+
+    if(isInsideBoard(moveX, moveY) && !visited[moveX][moveY]){
+
+      visited[moveX][moveY] = true;
+      queue.push([moveX, moveY]);
+
+    }
+  }
+
+    }    
+
+
+
 }
 
 const getMoves = (x, y) => {
@@ -18,8 +62,8 @@ const getMoves = (x, y) => {
 
 }
 
-const start = [3,3];
-const end = [4,4];
+const start = [0,0];
+const end = [3,3];
 
 minKnightMoves(start, end);
 
